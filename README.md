1d104b2b0662062b28613a8cb501369200adcebd
olektomek <olekstomek@gmail.com>
2020-10-06 22:21:40 +0200

    some improvements, bug fix

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ You can estimate the time according to PERT by entering an optimistic, normal an
 
 ![PERT_android](https://user-images.githubusercontent.com/26818304/63643447-8cd86580-c6d0-11e9-9f1e-4859b4ba8785.png)
 
+App idea from the book "The Clean Coder" (polish version "Mistrz czystego kodu") Robert C. Martin.
 
 You can only add numbers in the input field. The program will not allow you to enter, for example, an optimistic performance value greater than for a normal or pessimistic assumption, similarly for other fields, i.e. a field with a pessimistic value must have the highest value.
 The program allows tasks to be saved and the time is calculated for them. You can always delete the last task from the list or you can delete all tasks from the list.
--- pert/src/main/java/pl/olekstomek/pert/MainActivity.kt
+++ pert/src/main/java/pl/olekstomek/pert/MainActivity.kt
@@ -1,6 +1,5 @@
 package pl.olekstomek.pert
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
@@ -11,7 +10,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Math.pow
+import kotlin.math.pow
 import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
@@ -19,27 +18,33 @@ class MainActivity : AppCompatActivity() {
     private val standardDeviationResultsTimeTasksList = mutableListOf<Double>()
     private var firstSavingTasks = false
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val numberOfSavedTasks =
+            getString(R.string.number_of_saved_tasks, probabilityResultsTimeTasksList.size)
+        numberOfSavedTasksText.text = numberOfSavedTasks
 
         val probability = Probability()
         val standardDeviation = StandardDeviation()
+
         setTextNumberOfSavedTasks()
         calculateButton.setOnClickListener {
+            removeErrorMessageFromFields()
             getValuesFromInput(probability)
 
-            if (optimisticTimeInput.text.isEmpty() || normalTimeInput.text.isEmpty() || pessimisticTimeInput.text.isEmpty()) {
+            if (optimisticTimeInput.text.isEmpty() ||
+                normalTimeInput.text.isEmpty() ||
+                pessimisticTimeInput.text.isEmpty()
+            ) {
                 Toast.makeText(
                     this@MainActivity,
                     "Input all required values",
                     Toast.LENGTH_SHORT
                 ).show()
             } else {
-                val (validationOptimisticTime, validationNormalTime, validationPessimisticTime) = compareValuesInInput(
-                    probability
-                )
+                val (validationOptimisticTime, validationNormalTime, validationPessimisticTime) =
+                    compareValuesInInput(probability)
 
                 if (!validationOptimisticTime and !validationNormalTime and !validationPessimisticTime) {
                     val resultProbability = probability.calculateProbability(
@@ -54,61 +59,85 @@ class MainActivity : AppCompatActivity() {
                     )
 
                     addTasksToLists(resultProbability, resultStandardDeviation)
-
                     setTextNumberOfSavedTasks()
-
                     removeLastTaskInList()
-
                     removeAllTasksInList()
-
                     showResults(probability, resultProbability, resultStandardDeviation)
                 }
             }
         }
     }
 
+    private fun removeErrorMessageFromFields() {
+        optimisticTimeInput.error = null
+        normalTimeInput.error = null
+        pessimisticTimeInput.error = null
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
+
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
 
-        if (id == R.id.action_one) {
+        if (id == R.id.action_clear_all_fields) {
+            optimisticTimeInput.setText("")
+            normalTimeInput.setText("")
+            pessimisticTimeInput.setText("")
+
+            Toast.makeText(
+                this@MainActivity,
+                "All fields cleaned",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            return true
+        }
+
+        if (id == R.id.action_open_github) {
             startActivity(
                 Intent(
                     Intent.ACTION_VIEW,
                     Uri.parse("https://github.com/olekstomek/PERT")
                 )
             )
+
             return true
         }
+
         return super.onOptionsItemSelected(item)
     }
 
     private fun compareValuesInInput(probability: Probability): Triple<Boolean, Boolean, Boolean> {
-        val validationOptimisticTime =
-            probability.optimisticTime >= probability.normalTime || probability.optimisticTime >= probability.pessimisticTime
-        val validationNormalTime =
-            probability.normalTime <= probability.optimisticTime || probability.normalTime >= probability.pessimisticTime
-        val validationPessimisticTime =
-            probability.pessimisticTime <= probability.optimisticTime || probability.pessimisticTime <= probability.normalTime
+        val validationOptimisticTime = validateOptimisticTime(probability)
+        val validationNormalTime = validateNormalTime(probability)
+        val validationPessimisticTime = validatePessimisticTime(probability)
 
-        if (validationOptimisticTime) {
+        if (validationOptimisticTime)
             optimisticTimeInput.error =
-                "Optimistic time can not be greater/equal than normal time and greater/equal than than pessimistic time!"
-        }
-        if (validationNormalTime) {
+                "This can not be greater/equal than normal time and greater/equal than pessimistic time!"
+        if (validationNormalTime)
             normalTimeInput.error =
-                "Normal time can not be lower/equal than optimistic time and greater/equal than pessimistic time!"
-        }
-        if (validationPessimisticTime) {
+                "This can not be lower/equal than optimistic time and greater/equal than pessimistic time!"
+        if (validationPessimisticTime)
             pessimisticTimeInput.error =
-                "Pessimistic time can not be lower/equal than optimistic time and lower/equal than normal time!"
-        }
+                "This can not be lower/equal than optimistic time and lower/equal than normal time!"
+
         return Triple(validationOptimisticTime, validationNormalTime, validationPessimisticTime)
     }
+
+    private fun validatePessimisticTime(probability: Probability) =
+        probability.pessimisticTime <= probability.optimisticTime || probability.pessimisticTime <= probability.normalTime
+
+    private fun validateNormalTime(probability: Probability) =
+        probability.normalTime <= probability.optimisticTime || probability.normalTime >= probability.pessimisticTime
+
+    private fun validateOptimisticTime(probability: Probability) =
+        probability.optimisticTime >= probability.normalTime ||
+                probability.optimisticTime >= probability.pessimisticTime
 
     private fun showResults(
         probability: Probability,
@@ -118,8 +147,8 @@ class MainActivity : AppCompatActivity() {
         val dialogBuilder = AlertDialog.Builder(this)
         dialogBuilder.setMessage(
             "Values: [${probability.optimisticTime}, ${probability.normalTime}, ${probability.pessimisticTime}]\n" +
-                    "The expected duration of the task: " + "%.1f".format(resultProbability) + " days.\n" +
-                    "Standard deviation: " + "%.1f".format(resultStandardDeviation) + " days.\n\n" +
+                    "The expected duration of the task: " + "%.1f".format(resultProbability) + "\n" +
+                    "Standard deviation: " + "%.1f".format(resultStandardDeviation) + "\n\n" +
                     showTasksInMemoryOnResultsDialog()
         )
             .setCancelable(false)
@@ -162,15 +191,14 @@ class MainActivity : AppCompatActivity() {
     private fun showTasksInMemoryOnResultsDialog(): String {
         checkIfTheTasksAreSaved()
         return if (firstSavingTasks) {
-            "The expected duration of all " + probabilityResultsTimeTasksList.size + " tasks in memory: " + "%.1f".format(
+            "The expected duration of all " + probabilityResultsTimeTasksList.size + " tasks saved in memory: " + "%.1f".format(
                 sumAllProbabilityResultsTimeInList()
             ) +
-                    " days.\nStandard deviation of all " + standardDeviationResultsTimeTasksList.size + " tasks in memory: " + "%.1f".format(
+                    "\nStandard deviation of all " + standardDeviationResultsTimeTasksList.size + " tasks saved in memory: " + "%.1f".format(
                 sumAllStandardDeviationsResultsInList()
-            ) + " days."
-        } else {
-            "No saved results"
-        }
+            )
+        } else
+            "No saved results yet"
     }
 
     private fun checkIfTheTasksAreSaved() {
@@ -184,19 +212,19 @@ class MainActivity : AppCompatActivity() {
     private fun sumAllStandardDeviationsResultsInList(): Double {
         var sumOfDeviationsExponentiation = 0.0
         for (deviationItem in standardDeviationResultsTimeTasksList) {
-            sumOfDeviationsExponentiation += pow(deviationItem, 2.0)
+            sumOfDeviationsExponentiation += deviationItem.pow(2.0)
         }
         return sqrt(sumOfDeviationsExponentiation)
     }
 
-    @SuppressLint("SetTextI18n")
     private fun setTextNumberOfSavedTasks() {
-        if (!firstSavingTasks) {
+        if (!firstSavingTasks)
             checkIfTheTasksAreSaved()
-        }
+
         if (firstSavingTasks) {
-            numberOfSavedTasksText.text =
-                "Number of saved tasks: " + probabilityResultsTimeTasksList.size
+            val numberOfSavedTasks =
+                getString(R.string.number_of_saved_tasks, probabilityResultsTimeTasksList.size)
+            numberOfSavedTasksText.text = numberOfSavedTasks
         }
     }
 
